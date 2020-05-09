33b2efa2f18a4f3e55d9cd33df6700ee1e64051c
Rares Iordan <raresiordan09@gmail.com>
2020-05-09 16:24:05 +0300

    Updates

---
+++ precache-manifest.317d66e418d457a5b820787de8f99add.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6a7be36bcee01d4be61b0b03b71e7ede",
+    "url": "/covid-19-website/index.html"
+  },
+  {
+    "revision": "13d84795e29a5fee86e9",
+    "url": "/covid-19-website/static/css/2.ac75b6f8.chunk.css"
+  },
+  {
+    "revision": "982b403d4f21a746fd42",
+    "url": "/covid-19-website/static/css/main.232a6c62.chunk.css"
+  },
+  {
+    "revision": "13d84795e29a5fee86e9",
+    "url": "/covid-19-website/static/js/2.57650595.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/covid-19-website/static/js/2.57650595.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "982b403d4f21a746fd42",
+    "url": "/covid-19-website/static/js/main.681d08a9.chunk.js"
+  },
+  {
+    "revision": "bb20794135a820b6563e",
+    "url": "/covid-19-website/static/js/runtime-main.6b03faff.js"
+  },
+  {
+    "revision": "bbe18a415433f585a0b285105bae9ab2",
+    "url": "/covid-19-website/static/media/background-tile(1).bbe18a41.png"
+  },
+  {
+    "revision": "1b7fbe28b6b16210b5959e37604daf2f",
+    "url": "/covid-19-website/static/media/raleway-regular-webfont.1b7fbe28.woff2"
+  },
+  {
+    "revision": "812e6bf4eb8c4fd96fcf7636822cfe02",
+    "url": "/covid-19-website/static/media/raleway-regular-webfont.812e6bf4.ttf"
+  },
+  {
+    "revision": "fcc3a3ec34cd987b8f3c5e77e97a438c",
+    "url": "/covid-19-website/static/media/raleway-regular-webfont.fcc3a3ec.woff"
+  }
+]);
